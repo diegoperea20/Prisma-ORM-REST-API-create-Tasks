234075e1121b34b69908a5b85a4e19d5e5cdaa71
diegoperea20 <diegoperea20@gmail.com>
2024-05-10 15:51:22 -0500

    v1

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+import tasksRoutes from './routes/tasks.routes.js';
+const app = express();
+
+app.use(express.json());
+
+app.use('/',tasksRoutes)
+
+app.listen(3000)
+console.log("Server running on port 3000")
